1d0689b8ab04a7c64f2022bae58d12f4899a1a44
jucaneleo <ju.caneleo@duocuc.cl>
2023-03-16 17:27:03 -0300

    xmobar

--- xmonad/scripts/hdmi.sh
+++ xmonad/scripts/hdmi.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
 xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
-picom &
-nitrogen --restore &
-setxkbmap latam
-xinput --set-prop 17 'libinput Accel Speed' -0.7
-setxkbmap -option caps:escape
-
-
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,6 +7,8 @@ import XMonad
 import Data.Monoid
 import System.Exit
 import XMonad.Layout.Spacing
+import XMonad.Util.Run
+import XMonad.Hooks.ManageDocks
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
@@ -26,7 +28,7 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 --
-myBorderWidth   = 2
+myBorderWidth   = 1
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
@@ -182,7 +184,7 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full
+myLayout = avoidStruts tiled ||| Mirror tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
@@ -257,7 +259,10 @@ myStartupHook = do
 
 -- Run xmonad with the settings you specify. No need to modify this.
 --
-main = xmonad defaults
+main = do 
+	
+	xmproc <- spawnPipe "xmobar -x 0 /home/zamyaza/.config/xmobar/xmobarrc"
+	xmonad $ docks defaults
 
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
